458a55175632a8da02e54483014b4fd74998a234
daehiff <winderl13@gmail.com>
2021-04-25 13:29:40 +0200

    changed package name to sep

--- src/com/sep/Model.java
+++ src/com/sep/Model.java
@@ -1,4 +1,4 @@
-package com.company;
+package com.sep;
 
 import java.util.ArrayList;
 
--- src/com/sep/PrimaryViewModel.java
+++ src/com/sep/PrimaryViewModel.java
@@ -1,4 +1,4 @@
-package com.company;
+package com.sep;
 
 import java.io.BufferedReader;
 import java.io.IOException;
